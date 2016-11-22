609d302868f3d2cc6ffd54ac3d2acc4dfe7cea32
Jiwon Lee <madskaron@gmail.com>
2016-11-23 00:51:23 +1300

    Add data model for Uber APIs.

--- App1/App1/Views/TherePage.xaml.cs
+++ App1/App1/Views/TherePage.xaml.cs
@@ -6,11 +6,17 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
+using Plugin.Geolocator;
+
 
 namespace App1
 {
     public partial class TherePage : ContentPage
     {
+        public static double start_lat;
+        public static double start_long;
+        public static double end_lat= -36.783925;
+        public static double end_long= 174.754040;
 
         public TherePage()
         {
@@ -36,9 +42,22 @@ namespace App1
                 Label = "The Cafe",
                 Address = "74 Taharoto Rd, Takapuna"
             };
-            map.Pins.Add(pin);  
+            map.Pins.Add(pin);
+
+
 
             
+
+        }
+
+        public async void getUserLocation()    //user location의 GPS데이터 가져오기. 
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 50;
+
+            var userPosition = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+            start_lat = userPosition.Latitude;
+            start_long = userPosition.Longitude;
         }
 
 
